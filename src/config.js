b7515bfe6c2233e59e990976dd6f0bbd47a08d30
Khan Bifov <irestone.work@gmail.com>
2019-03-27 07:47:07 +0300

    Use .env

--- src/config.js
+++ src/config.js
@@ -1,2 +1,5 @@
-export const port = 8080
-export const env = 'development'
+const { NODE_ENV, PORT, MONGODB_URI } = process.env
+
+export const env = NODE_ENV || 'development'
+export const port = PORT || 8080
+export const dbURI = MONGODB_URI
